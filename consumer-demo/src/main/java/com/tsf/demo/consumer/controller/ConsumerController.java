dd03e292fc0448f72672e01691270b532172d927
Zijie Jiang <zijie.jiang@thoughtworks.com>
2025-01-15 14:42:08 +0800

    fix log path and print MDC

--- consumer-demo/src/main/java/com/tsf/demo/consumer/controller/ConsumerController.java
+++ consumer-demo/src/main/java/com/tsf/demo/consumer/controller/ConsumerController.java
@@ -4,6 +4,9 @@ import com.tencent.tsf.unit.TsfUnitContext;
 import com.tsf.demo.consumer.proxy.MeshUserService;
 import com.tsf.demo.consumer.proxy.ProviderDemoService;
 import com.tsf.demo.consumer.proxy.ProviderService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jboss.logging.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.tsf.core.TsfContext;
@@ -17,91 +20,96 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RestController
 public class ConsumerController {
-	@Autowired
-	private RestTemplate restTemplate;
-	@Autowired
-	private AsyncRestTemplate asyncRestTemplate;
-	@Autowired
-	private ProviderService providerService;
-	@Autowired
-	private ProviderDemoService providerDemoService;
-	@Autowired
-	private MeshUserService meshUserService;
+    private static final Logger log = LogManager.getLogger(ConsumerController.class);
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private AsyncRestTemplate asyncRestTemplate;
+    @Autowired
+    private ProviderService providerService;
+    @Autowired
+    private ProviderDemoService providerDemoService;
+    @Autowired
+    private MeshUserService meshUserService;
 
-	@RequestMapping(value = "/echo-rest/{str}", method = RequestMethod.GET)
-	public String restProvider(@PathVariable String str,
-							   @RequestParam(required = false) String tagName,
-							   @RequestParam(required = false) String tagValue) {
-		if (!StringUtils.isEmpty(tagName)) {
-			TsfContext.putTag(tagName, tagValue);
+    @RequestMapping(value = "/echo-rest/{str}", method = RequestMethod.GET)
+    public String restProvider(@PathVariable String str,
+                               @RequestParam(required = false) String tagName,
+                               @RequestParam(required = false) String tagValue) {
+        if (!StringUtils.isEmpty(tagName)) {
+            TsfContext.putTag(tagName, tagValue);
 //			TsfContext.putCustomMetadata(new CustomMetadata(tagName, tagValue));
-		}
-		return restTemplate.getForObject("http://provider-demo/echo/" + str, String.class);
-	}
+        }
+        return restTemplate.getForObject("http://provider-demo/echo/" + str, String.class);
+    }
 
-	@RequestMapping(value = "/echo-async-rest/{str}", method = RequestMethod.GET)
-	public String asyncRestProvider(@PathVariable String str,
-									@RequestParam(required = false) String tagName,
-									@RequestParam(required = false) String tagValue) throws Exception {
-		if (!StringUtils.isEmpty(tagName)) {
-			TsfContext.putTag(tagName, tagValue);
+    @RequestMapping(value = "/echo-async-rest/{str}", method = RequestMethod.GET)
+    public String asyncRestProvider(@PathVariable String str,
+                                    @RequestParam(required = false) String tagName,
+                                    @RequestParam(required = false) String tagValue) throws Exception {
+        if (!StringUtils.isEmpty(tagName)) {
+            TsfContext.putTag(tagName, tagValue);
 //			TsfContext.putCustomMetadata(new CustomMetadata(tagName, tagValue));
-		}
-		ListenableFuture<ResponseEntity<String>> future = asyncRestTemplate
-				.getForEntity("http://provider-demo/echo/" + str, String.class);
-		return future.get().getBody();
-	}
+        }
+        ListenableFuture<ResponseEntity<String>> future = asyncRestTemplate
+                .getForEntity("http://provider-demo/echo/" + str, String.class);
+        return future.get().getBody();
+    }
 
-	@RequestMapping(value = "/echo-feign/{str}", method = RequestMethod.GET)
-	public String feignProvider(@PathVariable String str,
-								@RequestParam(required = false) String tagName,
-								@RequestParam(required = false) String tagValue) {
-		if (!StringUtils.isEmpty(tagName)) {
-			TsfContext.putTag(tagName, tagValue);
+    @RequestMapping(value = "/echo-feign/{str}", method = RequestMethod.GET)
+    public String feignProvider(@PathVariable String str,
+                                @RequestParam(required = false) String tagName,
+                                @RequestParam(required = false) String tagValue) {
+        Map<String, Object> map = MDC.getMap();
+        log.info("receive str={}, tagName={}, tagValue={}", str, tagName, tagValue);
+        if (!StringUtils.isEmpty(tagName)) {
+            TsfContext.putTag(tagName, tagValue);
 //			TsfContext.putCustomMetadata(new CustomMetadata(tagName, tagValue));
-		}
-		return providerDemoService.echo(str);
-	}
+        }
+        return providerDemoService.echo(str);
+    }
 
-	@RequestMapping(value = "/echo-feign-url/{str}", method = RequestMethod.GET)
-	public String feignUrlProvider(@PathVariable String str,
-								   @RequestParam(required = false) String tagName,
-								   @RequestParam(required = false) String tagValue) {
-		if (!StringUtils.isEmpty(tagName)) {
-			TsfContext.putTag(tagName, tagValue);
+    @RequestMapping(value = "/echo-feign-url/{str}", method = RequestMethod.GET)
+    public String feignUrlProvider(@PathVariable String str,
+                                   @RequestParam(required = false) String tagName,
+                                   @RequestParam(required = false) String tagValue) {
+        if (!StringUtils.isEmpty(tagName)) {
+            TsfContext.putTag(tagName, tagValue);
 //			TsfContext.putCustomMetadata(new CustomMetadata(tagName, tagValue));
-		}
-		return providerService.echo(str);
-	}
+        }
+        return providerService.echo(str);
+    }
 
-	@RequestMapping(value = "/user-feign", method = RequestMethod.GET)
-	public String feignMeshUser(@RequestParam(required = false) String tagName,
-								@RequestParam(required = false) String tagValue) {
-		if (!StringUtils.isEmpty(tagName)) {
-			TsfContext.putTag(tagName, tagValue);
+    @RequestMapping(value = "/user-feign", method = RequestMethod.GET)
+    public String feignMeshUser(@RequestParam(required = false) String tagName,
+                                @RequestParam(required = false) String tagValue) {
+        if (!StringUtils.isEmpty(tagName)) {
+            TsfContext.putTag(tagName, tagValue);
 //			TsfContext.putCustomMetadata(new CustomMetadata(tagName, tagValue));
-		}
-		return meshUserService.create();
-	}
+        }
+        return meshUserService.create();
+    }
 
-	@RequestMapping(value = "/echo-feign-unit/{str}", method = RequestMethod.GET)
-	public String feignUnit(@PathVariable String str,
-							@RequestParam(required = false) String tagName,
-							@RequestParam(required = false) String tagValue) {
-		// 在feign方法上添加@TsfUnitRule注解
-		return providerDemoService.echoUnit(str);
-	}
+    @RequestMapping(value = "/echo-feign-unit/{str}", method = RequestMethod.GET)
+    public String feignUnit(@PathVariable String str,
+                            @RequestParam(required = false) String tagName,
+                            @RequestParam(required = false) String tagValue) {
+        // 在feign方法上添加@TsfUnitRule注解
+        return providerDemoService.echoUnit(str);
+    }
 
-	@RequestMapping(value = "/echo-rest-unit/{str}", method = RequestMethod.GET)
-	public String restUnit(@PathVariable String str,
-						   @RequestParam(required = false) String tagName,
-						   @RequestParam(required = false) String tagValue) {
-		if (!StringUtils.isEmpty(tagName)) {
-			// 添加单元化规则tag key和value
-			TsfUnitContext.putTag(tagName, tagValue);
-		}
-		return restTemplate.getForObject("http://provider-demo/echo/unit/" + str, String.class);
-	}
+    @RequestMapping(value = "/echo-rest-unit/{str}", method = RequestMethod.GET)
+    public String restUnit(@PathVariable String str,
+                           @RequestParam(required = false) String tagName,
+                           @RequestParam(required = false) String tagValue) {
+        if (!StringUtils.isEmpty(tagName)) {
+            // 添加单元化规则tag key和value
+            TsfUnitContext.putTag(tagName, tagValue);
+        }
+        return restTemplate.getForObject("http://provider-demo/echo/unit/" + str, String.class);
+    }
 }
